0c0ea6ecac71ccfe9a9a81b241e94e19014c3f96
Naseem Ullah <24660299+naseemkullah@users.noreply.github.com>
2022-05-20 12:30:26 -0400

    chore: deprecate autoLogging .ignorePaths and .getPaths in favor of .ignore

---
+++ deprecations.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const warning = require('process-warning')()
+
+const warnName = 'PinoHttpWarning'
+
+warning.create(warnName, 'PINOHTTP_DEP001', 'Use of "autoLogging.ignorePaths" is deprecated since version 7.1.0, use "opts.autoLogging.ignore" instead.')
+
+warning.create(warnName, 'PINOHTTP_DEP002', 'Use of "autoLogging.getPath" is deprecated since version 7.1.0, use "opts.autoLogging.ignore" instead.')
+
+module.exports = warning
