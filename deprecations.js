bbdae8e97bf0e5931a0ec3aa3d01743647502037
Luca Del Puppo <luca@delpuppo.net>
2022-06-14 14:26:41 +0200

    restore PINOHTTP_DEP002 warning

--- deprecations.js
+++ deprecations.js
@@ -5,5 +5,6 @@ const warning = require('process-warning')()
 // Warning example
 // const warnName = 'PinoHttpWarning'
 // warning.create(warnName, 'PINOHTTP_DEP001', 'Use of "autoLogging.ignorePaths" is deprecated since version 7.1.0, use "opts.autoLogging.ignore" instead.')
+// warning.create(warnName, 'PINOHTTP_DEP002', 'Use of "autoLogging.getPath" is deprecated since version 7.1.0, use "opts.autoLogging.ignore" instead.')
 
 module.exports = warning
