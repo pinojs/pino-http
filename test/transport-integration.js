31e11cf4da629324ea4b226ee1caaa9b182f82c5
James Sumners <git@zifbang.com>
2025-10-04 10:56:45 -0400

    Update for pino@10

--- test/transport-integration.js
+++ test/transport-integration.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const test = require('tap').test
-const http = require('http')
-const { join } = require('path')
+const test = require('node:test')
+const http = require('node:http')
+const { join } = require('node:path')
+const { spawn } = require('node:child_process')
+const tspl = require('@matteo.collina/tspl')
 
-const { spawn } = require('child_process')
-
-test('custom format transport', function (t) {
-  t.plan(2)
+test('custom format transport', async function (t) {
+  const plan = tspl(t, { plan: 2 })
 
   const ls = spawn('node', [join(__dirname, '../example-custom-format.js')], {
     cwd: process.cwd()
@@ -16,13 +16,15 @@ test('custom format transport', function (t) {
   ls.stderr.setEncoding('utf8')
 
   ls.stdout.on('data', (data) => {
-    t.match(data.trim(), /\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \+\d{4}\] GET http:\/\/.+:\d{4,6}\/ 200 \d{1,2}ms/)
+    plan.match(data.trim(), /\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \+\d{4}\] GET http:\/\/.+:\d{4,6}\/ 200 \d{1,2}ms/)
     ls.kill()
   })
 
   ls.stderr.on('data', (url) => {
-    t.ok(url.startsWith('http://'))
+    plan.ok(url.startsWith('http://'))
     http.get(url, () => {})
       .on('error', (e) => { t.error(e) })
   })
+
+  await plan
 })
